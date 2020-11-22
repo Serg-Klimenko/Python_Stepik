373336e46d9a6acee01392b22f15164de750c8ac
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-11-22 23:15:10 +0200

    task 3.2.1

--- part3.py
+++ part3.py
@@ -17,14 +17,41 @@
 # Функция не должна ничего возвращать, требуется только изменение переданного списка
 
 
-def modify_list(l):
-    for x in range(len(l) - 1, -1, -1):
-        if l[x] % 2 == 0:
-            l[x] = l[x] // 2
-        else:
-            l.pop(x)
+# def modify_list(l):
+#     for x in range(len(l) - 1, -1, -1):
+#         if l[x] % 2 == 0:
+#             l[x] = l[x] // 2
+#         else:
+#             l.pop(x)
+#
+#
+# lst = [10, 5, 8, 3]
+# modify_list(lst)
+# print(lst)
+
+# 3.2.1 Напишите функцию update_dictionary(d, key, value), которая принимает на вход словарь d
+# и два числа: key и value.
+# Если ключ key есть в словаре d, то добавьте значение value в список, который хранится по этому ключу.
+# Если ключа key нет в словаре, то нужно добавить значение в список по ключу 2∗key. Если и ключа
+# 2∗key нет, то нужно добавить ключ 2∗key в словарь и сопоставить ему список из переданного элемента [value].
+# Требуется реализовать только эту функцию, кода вне её не должно быть.
+# Функция не должна вызывать внутри себя функции input и print.
+
+def update_dictionary(d, key, value):
+    if d.get(key) is None:
+        k = 2 * key
+    else:
+        k = key
+    if d.get(k) is None:
+        d.update({k: [value]})
+    else:
+        d[k].append(value)
 
 
-lst = [10, 5, 8, 3]
-modify_list(lst)
-print(lst)
+d = {}
+print(update_dictionary(d, 1, -1))  # None
+print(d)  # {2: [-1]}
+update_dictionary(d, 2, -2)
+print(d)  # {2: [-1, -2]}
+update_dictionary(d, 1, -3)
+print(d)  # {2: [-1, -2, -3]}
