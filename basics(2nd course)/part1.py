77f47b3e3645f634b3551d5314b9175dfb2c9cf0
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-12-06 19:25:21 +0200

    task 1.6.2

--- basics(2nd course)/part1.py
+++ basics(2nd course)/part1.py
@@ -166,7 +166,6 @@
 #         print(result)
 # ------------------------------------------------------------------------------------------------
 # 1.5.1
-#
 # Реализуйте класс MoneyBox, для работы с виртуальной копилкой.
 # Каждая копилка имеет ограниченную вместимость, которая выражается целым числом – количеством
 # монет, которые можно положить в копилку. Класс должен поддерживать информацию о количестве
@@ -299,28 +298,79 @@
 # Формат выходных данных
 # Для каждого запроса выведите в отдельной строке слово "Yes", если класс 1 является предком
 # класса 2, и "No", если не является.
-classes = {}
+# classes = {}
+#
+#
+# def is_parent(parent, child):
+#     if child in classes.keys():
+#         if child == parent or parent in classes[child]:
+#             return True
+#         elif not classes[child]:
+#             return False
+#         else:
+#             for j in classes[child]:
+#                 if j in classes.keys() and is_parent(parent, j):
+#                     return True
+#     return False
+#
+#
+# for i in range(int(input())):
+#     line = input().replace(':', ' ').split()
+#     classes.setdefault(line[0], line[1:])
+# for i in range(int(input())):
+#     parent, child = input().split()
+#     if is_parent(parent, child):
+#         print('Yes')
+#     else:
+#         print('No')
+# -------------------------------------------------------------------------------------------
+
+# 1.6.2 Наследование классов
+# Реализуйте структуру данных, представляющую собой расширенную структуру стек.
+# Необходимо поддерживать добавление элемента на вершину стека, удаление с вершины стека,
+# и необходимо поддерживать операции сложения, вычитания, умножения и целочисленного деления.
+# Операция сложения на стеке определяется следующим образом. Со стека снимается верхний
+# элемент (top1), затем снимается следующий верхний элемент (top2), и затем как результат
+# операции сложения на вершину стека кладется элемент, равный top1 + top2.
+# Аналогичным образом определяются операции вычитания (top1 - top2), умножения (top1 * top2)
+# и целочисленного деления (top1 // top2).
+# Реализуйте эту структуру данных как класс ExtendedStack, отнаследовав его от стандартного
+# класса list.
+# Требуемая структура класса:
+# class ExtendedStack(list):
+#     def sum(self):
+#         # операция сложения
+#     def sub(self):
+#         # операция вычитания
+#     def mul(self):
+#         # операция умножения
+#     def div(self):
+#         # операция целочисленного деления
+# Примечание
+# Для добавления элемента на стек используется метод append, а для снятия со стека – метод pop
+# Гарантируется, что операции будут совершаться только когда в стеке есть хотя бы два элемента
+
+class ExtendedStack(list):
+    def sum(self):  # операция сложения
+        self.append(self.pop() + self.pop())
+
+    def sub(self):  # операция вычитания
+        self.append(self.pop() - self.pop())
+
+    def mul(self):  # операция умножения
+        self.append(self.pop() * self.pop())
+
+    def div(self):  # операция целочисленного деления
+        self.append(self.pop() // self.pop())
 
 
-def is_parent(parent, child):
-    if child in classes.keys():
-        if child == parent or parent in classes[child]:
-            return True
-        elif not classes[child]:
-            return False
-        else:
-            for j in classes[child]:
-                if j in classes.keys() and is_parent(parent, j):
-                    return True
-    return False
-
-
-for i in range(int(input())):
-    line = input().replace(':', ' ').split()
-    classes.setdefault(line[0], line[1:])
-for i in range(int(input())):
-    parent, child = input().split()
-    if is_parent(parent, child):
-        print('Yes')
-    else:
-        print('No')
+ex_stack = ExtendedStack([1, 2, 3, 4, -3, 3, 5, 10])
+ex_stack.div()
+assert ex_stack.pop() == 2
+ex_stack.sub()
+assert ex_stack.pop() == 6
+ex_stack.sum()
+assert ex_stack.pop() == 7
+ex_stack.mul()
+assert ex_stack.pop() == 2
+assert len(ex_stack) == 0
