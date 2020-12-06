05a33ff24215e416096706da635ae733ad68993a
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-12-06 15:59:40 +0200

    training 9.1 Durak

--- training/main.py
+++ training/main.py
@@ -154,14 +154,48 @@
 #
 # Формат вывода:
 # Строка, содержащая закодированную последовательность.
-s = input()
-l = len(s)
-i = 0
-while i < l:
-    cnt = 1
-    while i + cnt < l and s[i] == s[i + cnt]:
-        cnt += 1
-    if cnt > 1:
-        print(cnt, end='')
-    print(s[i], end='')
-    i += cnt
+# s = input()
+# l = len(s)
+# i = 0
+# while i < l:
+#     cnt = 1
+#     while i + cnt < l and s[i] == s[i + cnt]:
+#         cnt += 1
+#     if cnt > 1:
+#         print(cnt, end='')
+#     print(s[i], end='')
+#     i += cnt
+# ---------------------------------------------------------------------------------------------
+
+# 9.1 Durak
+# A durak deck contains 36 cards. Each card has a suit of either clubs, diamonds, hearts,
+# or spades (denoted C, D, H, S). Each card also has a value of either 6 through 10, jack,
+# queen, king, or ace (denoted 6, 7, 8, 9, 10, J, Q, K, A). For scoring purposes card values
+# are ordered as above, with 6 having the lowest and ace the highest value.
+# # Напишите программу, которая определяет, бьёт ли одна карта другую.
+# Если встречаются две карты одной масти, то побеждает та, у которой выше значение;
+# Если карты разных мастей, то карта, имеющая козырную масть, побеждает;
+# Если карты разных мастей и нет козырных, то никто не побеждает.
+# # Формат ввода:
+# На первой строке через пробел указываются две карты в формате <значение><масть>, а на
+# следующей строке указывается козырная масть.
+# # Формат вывода:
+# Программа должна вывести слово
+# First, если первая карта бьёт вторую,
+# Second, если вторая карта бьёт первую,
+# Error, если ни одна из карт не может побить другую.
+deck = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+result = 'Error'
+first, second = input().split()
+suit = input()
+if first[-1] == second[-1]:
+    if deck.index(first[:-1]) - deck.index(second[:-1]) > 0:
+        result = 'First'
+    else:
+        result = 'Second'
+else:
+    if first[-1] == suit:
+        result = 'First'
+    elif second[-1] == suit:
+        result = 'Second'
+print(result)
