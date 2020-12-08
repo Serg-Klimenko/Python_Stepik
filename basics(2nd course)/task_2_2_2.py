1c956403040e9df270bc17c7785a15e6c2a7dbe5
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-12-08 23:50:36 +0200

    task 2.2.2

--- basics(2nd course)/task_2_2_2.py
+++ basics(2nd course)/task_2_2_2.py
@@ -5,11 +5,16 @@
 # Вам необходимо установить библиотеку simple-crypt, и с помощью метода simplecrypt.decrypt узнать,
 # какой из паролей служит ключом для расшифровки файла с интересной информацией.
 # Ответом для данной задачи служит расшифрованная интересная информация Алисы.
-from simplecrypt import decrypt
-# with open("passwords.txt", "r") as inf_password:
-#     passwords = inf_password.read().split('\n')
+from simplecrypt import decrypt, DecryptionException
+with open("passwords.txt", "r") as inf_password:
+     passwords = inf_password.read().split('\n')
 with open("encrypted.bin", "rb") as inf_data:
     encrypted = inf_data.read()
-# for password in passwords:
-password = 'RVrF2qdMpoq6Lib'
-print(decrypt(password, encrypted))
+for password in passwords:
+# password = 'RVrF2qdMpoq6Lib'
+    try:
+        print(decrypt(password, encrypted))
+        break
+    except DecryptionException:
+        pass
+
