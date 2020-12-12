2e6ae5f06ac8c51ce4a58473375e4ae569ce2d7a
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-12-12 16:50:02 +0200

    task 2.3.1 iterator

--- basics(2nd course)/task_2_3_1.py
+++ basics(2nd course)/task_2_3_1.py
@@ -5,9 +5,9 @@
 # f допускает элемент x, а элемент x является допущенным.
 # В данной задаче мы просим вас реализовать класс multifilter, который будет выполнять ту же функцию,
 # что и стандартный класс filter, но будет использовать не одну функцию, а несколько.
-# # Решение о допуске элемента будет приниматься на основании того, сколько функций допускают этот элемент,
+# Решение о допуске элемента будет приниматься на основании того, сколько функций допускают этот элемент,
 # и сколько не допускают. Обозначим эти количества за pos и neg.
-# # Введем понятие решающей функции – это функция, которая принимает два аргумента – количества pos и neg,
+# Введем понятие решающей функции – это функция, которая принимает два аргумента – количества pos и neg,
 # и возвращает True, если элемент допущен, и False иначе.
 #
 # Рассмотрим процесс допуска подробнее на следующем примере.
@@ -34,40 +34,75 @@
 # judge_any(1, 1) равно True, значит мы допускаем элемент x = 2.
 # Аналогично для третьего элемента x = 3.
 # Таким образом, получили последовательность допущенных элементов [2, 3].
-class multifilter:
+class Multifilter:
     def judge_half(pos, neg):
         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
+        if pos >= neg:
+            return True
+        return False
 
     def judge_any(pos, neg):
         # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+        if pos >= 1:
+            return True
+        return False
 
     def judge_all(pos, neg):
         # допускает элемент, если его допускают все функции (neg == 0)
+        if neg == 0:
+            return True
+        return False
 
     def __init__(self, iterable, *funcs, judge=judge_any):
+        self.i = 0
         # iterable - исходная последовательность
+        self.iterable = iterable
         # funcs - допускающие функции
+        self.funcs = funcs
         # judge - решающая функция
+        self.judge = judge
 
     def __iter__(self):
         # возвращает итератор по результирующей последовательности
+        return self
+
+    def __next__(self):
+        if self.i < len(self.iterable):
+            self.i += 1
+            pos = 0
+            neg = 0
+            for x in self.funcs:
+                if x(self.iterable[self.i-1]):
+                    pos += 1
+                else:
+                    neg += 1
+            if self.judge(pos, neg):
+                return self.iterable[self.i-1]
+            else:
+                return self.__next__()
+        else:
+            raise StopIteration
+
 
 def mul2(x):
     return x % 2 == 0
 
+
 def mul3(x):
     return x % 3 == 0
+
 
 def mul5(x):
     return x % 5 == 0
 
+
 a = [i for i in range(31)]  # [0, 1, 2, ... , 30]
 
-print(list(multifilter(a, mul2, mul3, mul5)))
+print(list(Multifilter(a, mul2, mul3, mul5)))
 # [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
 
-print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_half)))
+print(list(Multifilter(a, mul2, mul3, mul5, judge=Multifilter.judge_half)))
 # [0, 6, 10, 12, 15, 18, 20, 24, 30]
 
-print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_all)))
+print(list(Multifilter(a, mul2, mul3, mul5, judge=Multifilter.judge_all)))
 # [0, 30]
