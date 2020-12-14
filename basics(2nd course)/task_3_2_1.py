0e265149d51c9064c989b0ac55e3ecc7be8c8ef7
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-12-14 23:27:19 +0200

    task 3.2.2 RegExp

--- basics(2nd course)/task_3_2_1.py
+++ basics(2nd course)/task_3_2_1.py
@@ -1,10 +1,15 @@
 # 3.2.1 Регулярные выражения
 # Вам дана последовательность строк.
 # Выведите строки, содержащие "cat" в качестве подстроки хотя бы два раза.
+# 3.2.2
+# Выведите строки, содержащие "cat" в качестве слова.
+# Примечание: Для работы со словами используйте группы символов \b и \B.
 import re
 import sys
 
 for line in sys.stdin:
     line = line.rstrip()
-    if re.search(r"(cat.*){2}", line) is not None:
+    pattern_task1 = r"(cat.*){2}"
+    pattern_task2 = r"\bcat\b"
+    if re.search(pattern_task2, line) is not None:
         print(line)
